a89103a71c088c93a1abfaf96ed8629ef5e84d18
vandiep65 <vandiep7a1@gmail.com>
2021-04-10 11:29:06 +0700

    -m 'i finished it'

--- main.m
+++ main.m
@@ -30,21 +30,49 @@ A = [a b];
 Df = 10^(-4);
 chiadoi(f,A,Df)
 
+disp('KET QUA BAI 6 VA BAI 7 NHU HINH');
 %Bai 6
 syms x
 
 disp('Ket qua bai 6 cau a, b: Da viet function');
-disp('Ket qua bai 6 cau c la: ');
 f = x + sin(x) -2;
 PI = 2 - sin(x);
 x0 = 1.05;
 Df = 10^(-3);
-lap(f, PI, x0, Df);
+lap(f, PI, x0, Df,"Bai 6 cau c");
 
-disp('Ket qua bai 6 cau d la: ');
+disp('Ket qua bai 6 d la: ');
+disp('Neu su dung PI = 5-x^2 thi f(x) se cang ngay cang tang, vi dao ham cua PI >1. Doi lai PI = sqrt(5-x) nhu ben duoi, ta duoc: ');
 f = x^2+x-5;
-PI = 5 - x^2;
+PI = sqrt(5-x);
 x0 = 1.5;
 Df = 10^(-3);
-lap(f, PI, x0, Df);
+lap(f, PI, x0, Df,"Bai 6 cau d");
+
+%Bai 7
+syms x;
+f = x-x/2-1/x;
+PI = x/2+1/x;
+
+%Cau a
+% Bai 7 cau a
+x0 =1;
+Df = 10^(-3);
+lap(f,PI,x0,Df,"Bai 7 cau a");
+
+% Bai 7 cau b
+x0 =2;
+Df = 3*10^(-3);
+lap(f,PI,x0,Df,"Bai 7 cau b");
+
+% Bai 7 cau c
+x0 =-2;
+Df = 10^(-2);
+lap(f,PI,x0,Df,"Bai 7 cau c");
+
+% Bai 7 cau d
+x0 =-5;
+Df = 10^(-4);
+lap(f,PI,x0,Df,"Bai 7 cau d");
+
 
