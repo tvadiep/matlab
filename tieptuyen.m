ac925f8e5b21c849302f2d259db600983fe7b259
vandiep65 <vandiep7a1@gmail.com>
2021-04-12 10:02:39 +0700

    'first commit'

--- tieptuyen.m
+++ tieptuyen.m
@@ -1,9 +1,12 @@
 function[SOL] = tieptuyen(f,x0,Df)
 syms x;
+k = 0;
 while subs(f,x,x0) > Df
+    k =k +1;
+    disp(k);
     gt_f = subs(f,x,x0);
     gt_df = subs(diff(f,x),x,x0);
     x0 = x0 - gt_f/gt_df;
 end
-SOL = x0;
+SOL = x0
 end
